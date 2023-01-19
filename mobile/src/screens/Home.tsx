344eb1025fdc2ecf5c94d690760b1adbe742a027
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-01-18 22:01:01 -0300

    feat: generate dates from year begin in mobile

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,8 +1,13 @@
-import { Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+
 import { DAY_SIZE, HabitDay } from '../components/HabitDay';
 import { Header } from '../components/Header';
+import { generateDatesFromYearBegin } from '../utils/generate-dates-from-year-begin';
 
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'];
+const datesFromYearBegin = generateDatesFromYearBegin();
+const minimumSummaryDatesSizes = 10 * 7;
+const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearBegin.length;
 
 export function Home() {
   return (
@@ -21,7 +26,25 @@ export function Home() {
         ))}
       </View>
 
-      <HabitDay />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        <View className="flex-row flex-wrap">
+          {datesFromYearBegin.map((date) => (
+            <HabitDay key={date.toISOString()} />
+          ))}
+
+          {amountOfDaysToFill > 0 &&
+            Array.from({ length: amountOfDaysToFill }).map((_, index) => (
+              <View
+                key={index}
+                className="bg-zinc-900 rounded-lg border-2 m-1 border-zinc-800 opacity-60"
+                style={{ width: DAY_SIZE, height: DAY_SIZE }}
+              />
+            ))}
+        </View>
+      </ScrollView>
     </View>
   );
 }
