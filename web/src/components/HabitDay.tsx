eddaf65d0797523a1384b2bf99b88b2b5c332d62
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-01-21 19:27:01 -0300

    feat: web - new habit form checkbox

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -33,54 +33,57 @@ export function HabitDay({ amount, completed }: HabitProps) {
 
           'bg-violet-500 border-violet-400': completedPercentage >= 80,
         })}
-      >
-        <Popover.Portal>
-          <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
-            <span className="font-semibold text-zinc-400">sexta-feira</span>
-            <span className="mt-1 font-extrabold leading-tight text-3xl">
-              20/01
-            </span>
+      />
+      <Popover.Portal>
+        <Popover.Content className="min-w-[320px] p-6 rounded-2xl bg-zinc-900 flex flex-col">
+          <span className="font-semibold text-zinc-400">sexta-feira</span>
+          <span className="mt-1 font-extrabold leading-tight text-3xl">
+            20/01
+          </span>
 
-            <ProgressBar progress={completedPercentage} />
+          <ProgressBar progress={completedPercentage} />
 
-            <div className="mt-6 flex flex-col gap-3">
-              <Checkbox.Root className="flex items-center gap-3 group">
-                <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 rounded-lg group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                  <Checkbox.Indicator>
-                    <Check size={20} className="text-white" />
-                  </Checkbox.Indicator>
-                </div>
+          <div className="mt-6 flex flex-col gap-3">
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 rounded-lg group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
 
-                <span className="font-semibold text-xl text-white leading-tight">
-                  Beber 2L de água
-                </span>
-              </Checkbox.Root>
+              <span className="font-semibold text-xl text-white leading-tight">
+                Beber 2L de água
+              </span>
+            </Checkbox.Root>
 
-              <Checkbox.Root className="flex items-center gap-3 group">
-                <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 rounded-lg group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                  <Checkbox.Indicator />
-                </div>
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 rounded-lg group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
 
-                <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
-                  Caminhar
-                </span>
-              </Checkbox.Root>
+              <span className="font-semibold text-xl text-white leading-tight group-data-[state=checked]:line-through group-data-[state=checked]:text-zinc-400">
+                Caminhar
+              </span>
+            </Checkbox.Root>
 
-              <Checkbox.Root className="flex items-center gap-3 group">
-                <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 rounded-lg group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
-                  <Checkbox.Indicator />
-                </div>
+            <Checkbox.Root className="flex items-center gap-3 group">
+              <div className="h-8 w-8 flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 rounded-lg group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500">
+                <Checkbox.Indicator>
+                  <Check size={20} className="text-white" />
+                </Checkbox.Indicator>
+              </div>
 
-                <span className="font-semibold text-xl text-white leading-tight">
-                  Codar
-                </span>
-              </Checkbox.Root>
-            </div>
+              <span className="font-semibold text-xl text-white leading-tight">
+                Codar
+              </span>
+            </Checkbox.Root>
+          </div>
 
-            <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
-          </Popover.Content>
-        </Popover.Portal>
-      </Popover.Trigger>
+          <Popover.Arrow height={8} width={16} className="fill-zinc-900" />
+        </Popover.Content>
+      </Popover.Portal>
     </Popover.Root>
   );
 }
