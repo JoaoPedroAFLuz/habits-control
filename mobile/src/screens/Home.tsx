1cb3fdc3771735b11a083a36b75edfc87afe7fcd
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-01-21 15:31:49 -0300

    feat: mobile - habit day and progress bar component

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { Text, View, ScrollView } from 'react-native';
 
 import { DAY_SIZE, HabitDay } from '../components/HabitDay';
@@ -10,6 +11,8 @@ const minimumSummaryDatesSizes = 10 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesFromYearBegin.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
+
   return (
     <View className="flex-1 bg-black px-8 pt-16">
       <Header />
@@ -32,7 +35,10 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearBegin.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => navigate('habit', { date: date.toISOString() })}
+            />
           ))}
 
           {amountOfDaysToFill > 0 &&
