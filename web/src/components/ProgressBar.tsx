d014e24657ceac79fdc320a91d05e86ac14c97fd
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-01-23 19:58:40 -0300

    feat: web - add transitions and focus

--- web/src/components/ProgressBar.tsx
+++ web/src/components/ProgressBar.tsx
@@ -8,7 +8,7 @@ export function ProgressBar({ progress }: ProgressBarProps) {
   return (
     <Progress.Root className="w-full mt-4 h-3 rounded-xl bg-zinc-700">
       <Progress.Indicator
-        className="h-full rounded-xl bg-violet-600"
+        className="h-full rounded-xl bg-violet-600 transition-all"
         style={{ width: `${progress}%` }}
       />
     </Progress.Root>
