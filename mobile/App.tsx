081b8ee18ad5c67803a3aa11e7a1ce896accfc13
João Pedro Luz <joao.pedro.luz@hotmail.com>
2023-01-26 22:04:33 -0300

    feat: mobile - send reminders notifications

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,7 @@
 import './src/lib/dayjs';
 
+import * as Notifications from 'expo-notifications';
+import { useEffect } from 'react';
 import { StatusBar } from 'react-native';
 
 import {
@@ -13,6 +15,16 @@ import {
 import { Loading } from './src/components/Loading';
 import { Routes } from './src/routes';
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
+
+Notifications.getPermissionsAsync();
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Inter_400Regular,
@@ -20,6 +32,29 @@ export default function App() {
     Inter_700Bold,
     Inter_800ExtraBold,
   });
+
+  async function schedulePushNotification() {
+    const schedule = await Notifications.getAllScheduledNotificationsAsync();
+
+    if (schedule.length > 0) {
+      await Notifications.cancelAllScheduledNotificationsAsync();
+    }
+
+    const trigger = new Date(Date.now());
+    trigger.setSeconds(trigger.getSeconds() + 5);
+
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: 'Olá 😀',
+        body: 'Você praticou seus hábitos hoje?',
+      },
+      trigger,
+    });
+  }
+
+  useEffect(() => {
+    schedulePushNotification();
+  }, []);
 
   if (!fontsLoaded) {
     return <Loading />;
@@ -32,7 +67,6 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-
       <Routes />
     </>
   );
